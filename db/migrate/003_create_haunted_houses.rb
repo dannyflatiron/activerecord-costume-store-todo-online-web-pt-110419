87b63038ccb1abb4eab9e0cb5e911b654f229e08
Daniel Reina <dannyflatiron@gmail.com>
2020-02-05 04:10:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,11 +1,14 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
-  # def change
-  #   create_table :haunted_houses do |t|
-  #     t.string :name
-  #   end
-  # end
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.value
+    end
+  end
 
 
 end
