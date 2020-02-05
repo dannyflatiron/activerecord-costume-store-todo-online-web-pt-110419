0b0a8615a9ba4f87a24e64adc47542e84df9dd90
Daniel Reina <dannyflatiron@gmail.com>
2020-02-05 04:13:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,11 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.value
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
     end
   end
 
